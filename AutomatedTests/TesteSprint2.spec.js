8c72e501d17f4d1aa30d6fc6208939d913091c6b
Rodrigo Daniel <119114888+rodrigodslima@users.noreply.github.com>
2025-05-25 10:45:42 -0300

    Add files via upload

---
+++ AutomatedTests/TesteSprint2.spec.js
@@ -0,0 +1,47 @@
+/// <reference types="cypress" />
+
+
+describe("Testes automatizados com cypress Sprint 2", () => {
+
+    beforeEach(()=> {
+        cy.visit("https://www.saucedemo.com/v1/")
+        
+})
+
+
+    it('Testar a funcionalidade da opção de buscar produtos de A a Z', () => {
+
+        cy.get('[data-test=username]').type("standard_user").should("have.value","standard_user")
+        cy.get('[data-test=password]').type("secret_sauce").should("have.value","secret_sauce")
+        cy.get('#login-button').click()
+        cy.get('.product_sort_container').select('az').should('contain','Name (A to Z)')
+       
+    })
+
+    it('Testar a funcionalidade da opção de buscar produtos de Z a A', () => {
+
+        cy.get('[data-test=username]').type("standard_user").should("have.value","standard_user")
+        cy.get('[data-test=password]').type("secret_sauce").should("have.value","secret_sauce")
+        cy.get('#login-button').click()
+        cy.get('.product_sort_container').select('za').should('contain','Name (Z to A)')
+
+    }) 
+
+    it('Testar a funcionalidade da opção de buscar produtos pelo preço mais baixo', () => {
+
+        cy.get('[data-test=username]').type("standard_user").should("have.value","standard_user")
+        cy.get('[data-test=password]').type("secret_sauce").should("have.value","secret_sauce")
+        cy.get('#login-button').click()
+        cy.get('.product_sort_container').select('lohi').should('contain','Price (low to high')               
+    })
+
+    it('Testar a funcionalidade da opção de buscar produtos pelo preço mais alto', () => {
+
+        cy.get('[data-test=username]').type("standard_user").should("have.value","standard_user")
+        cy.get('[data-test=password]').type("secret_sauce").should("have.value","secret_sauce")
+        cy.get('#login-button').click()
+        cy.get('.product_sort_container').select('hilo').should('contain','Price (high to low')
+
+    })    
+
+})
